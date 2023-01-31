6f3bca51ce9b959b7e95ec600555658f3ba521af
MKrylov-2022 <mishacat_2015@mail.ru>
2023-01-31 13:59:57 +0700

    Пётр

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print('bruh')
+'''print('bruh')
 print(' ноу факинг вей, оно воркинг') # вместо Hello World
 print('фор рил мен')
+'''
+
+
+def partial_sums(*numbers):
+    result = [0]
+    Oleg = list([*numbers])
+    for i in range(len(Oleg)):
+        result.append(sum(Oleg[:i + 1]))
+    print(result)
+
+partial_sums()
