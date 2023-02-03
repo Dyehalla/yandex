9519ffda6fbcbe74bae134f55661193588b013fd
IMikhaylov-2022 <igmih206@gmail.com>
2023-02-03 17:54:42 +0700

    аннаграмы

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-def ask_password(login, password, success, failure):
-    vowels_lst = {'e', 'a', 'o', 'u', 'i', 'u', 'y'}
-    vowels = 0
-    for sym in password:
-        if sym in vowels_lst:
-            vowels += 1
-    login_cons = ''
-    password_cons = ''
-    for sym in login:
-        if sym.isalpha() and not sym in vowels_lst:
-            login_cons += sym
+words_amount = int(input())
+words = list()
+result = list()
+for _ in range(words_amount):
+    words.append(input().lower())
 
-    for sym in password:
-        if sym.isalpha() and not sym in vowels_lst:
-            password_cons += sym
+for word1 in words:
+    first_word = True
+    for word2 in words:
+        if words.index(word1) != words.index(word2):
+            if sorted(list(word1)) == sorted(list(word2)):
+                if first_word:
+                    result.append([word1, word2])
+                    first_word = False
+                    index = len(result) - 1
+                else:
+                    result[index].append(word2)
+        if not first_word:
+            result[index].sort()
+    for i in result[index]:
+        words.remove(i)
 
-    if login_cons == password_cons and vowels == 3:
-        success()
-        return True
-    else:
-        failure()
-        return False
 
-
-def main(login, password):
+for res in sorted(result, key=lambda lst: lst[0]):
+    print(*res)
