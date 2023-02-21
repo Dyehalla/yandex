c0065af37af8f16dc191e7fa65e2da5145e62cf4
IMikhaylov-2022 <igmih206@gmail.com>
2023-02-21 17:23:10 +0700

    zеркало

--- Misha.py
+++ Misha.py
@@ -1,26 +1,15 @@
-'''words = []
-keys = []
-another_words = []
-result = []
-for n in range(int(input())):
-    words.append(input().lower())
-for i in words:
-    if sorted(list(i)) in keys:
-        another_words[keys.index(sorted(list(i)))].append(i)
-    else:
-        keys.append(sorted(list(i)))
-        another_words.append([])
-        another_words[keys.index(sorted(list(i)))].append(i)
+from PIL import Image, ImageDraw
 
-for i in another_words:
-    sorted(i)
-    print(*i)'''
+im = Image.open("lesson-26-3.jpg")
+pixels = im.load()
 
+x, y = im.size
 
-def tribonacci(n):
-    if 0 <= n <= 1:
-        return 0
-    elif 2 <= n <= 3:
-        return 1
-    else:
-        return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3)
+my_image = Image.new("RGB", (x, y), (0, 0, 0))
+new_pixels = my_image.load()
+
+for i_y in range(y):
+    for i_x in range(x):
+        new_pixels[i_x, i_y] = pixels[x - i_x - 1, i_y]
+
+my_image.save('dammit.jpg')
