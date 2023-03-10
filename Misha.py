36d3169015c4d8167e1bb1c0c7b8bdf999fa2252
sinorin <mishacat_2015@mail.com>
2023-03-10 11:01:42 +0800

    BeeBa

--- Misha.py
+++ Misha.py
@@ -16,20 +16,33 @@ for i in another_words:
     sorted(i)
     print(*i)'''
 
-
-def is_hypersimple(number):
-    for i in range(2, number):
-        if number % i == 0:
-            return False
-    return True
+import pymorphy2
+from sys import stdin
 
 
-number = int(input())
-while (number != 0):
-    if is_hypersimple(number):
-        number //= 10
-    else:
-        print("NO")
-        break
-else:
-    print("YES")
+def pls_return_stripped_word(word):
+    output = ''
+    for letter in word:
+        if letter.isalpha():
+            output = output + letter
+    return output.lower()
+
+
+user_input = [i for i in stdin]
+found = []
+nouns = []
+morph = pymorphy2.MorphAnalyzer()
+for sentence in user_input:
+    for word in sentence.split():
+        for variant in morph.parse(pls_return_stripped_word(word)):
+            if variant.score > 0.5 and variant.tag.POS == "NOUN":
+                if variant.normal_form not in found:
+                    found.append(variant.normal_form)
+                    nouns.append([variant.normal_form, 1])
+                    break
+                else:
+                    nouns[found.index(variant.normal_form)][1] += 1
+                    break
+nouns.sort(key=(lambda x: (x[1], x[0])), reverse=True)
+for words in nouns[:10]:
+    print(words[0], end=" ")
